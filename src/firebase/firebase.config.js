2cf8afbfc51ce2acd4dabfea041a9160b8c127cb
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-04-25 21:24:22 +0600

    secure firebase Keys

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -7,13 +7,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC023qTNBm6FUoRhl6agsy-Kw0XhLska44",
-  authDomain: "assignment-ten-3e753.firebaseapp.com",
-  projectId: "assignment-ten-3e753",
-  storageBucket: "assignment-ten-3e753.appspot.com",
-  messagingSenderId: "143592334116",
-  appId: "1:143592334116:web:869e079eeef4b8f4ca89b1",
-  measurementId: "G-D932CS6X9N"
+  apiKey: import.meta.env.VITE-APIKEY,
+  authDomain: import.meta.env.VITE-AUTHDOMAIN,
+  projectId: import.meta.env.VITE-PROJECTID,
+  storageBucket: import.meta.env.VITE-STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE-MESSAGINGSENDERID,
+  appId: import.meta.env.VITE-APPID,
+  measurementId: import.meta.env.VITE-MEASUREMENTID,
 };
 
 // Initialize Firebase
