b94b271c0bfd73b17a2e9b8714b30070e0139dbe
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-04-29 20:53:29 +0600

    typewriter and react-awesome-reveal used

--- src/components/OurTeam.jsx
+++ src/components/OurTeam.jsx
@@ -1,11 +1,14 @@
+import { Slide } from "react-awesome-reveal";
 
 
 const OurTeam = () => {
     return (
+
         <section className="py-6 dark:bg-gray-100 dark:text-gray-800 mt-20">
         <div className="container flex flex-col items-center justify-center p-4 mx-auto space-y-8 sm:p-10">
             <h1 className="text-4xl font-bold leading-none text-center sm:text-5xl">Our team</h1>
             <p className="max-w-2xl text-center dark:text-gray-600 text-3xl">Together, we make the perfect team and Achieve the impossible.</p>
+            <Slide>
             <div className="flex flex-row flex-wrap-reverse justify-center">
                 <div className="flex flex-col justify-center m-8 text-center">
                     <img alt="" className="self-center flex-shrink-0 w-24 h-24 mb-4 bg-center bg-cover rounded-full dark:bg-gray-500 object-cover" src="https://i.ibb.co/VQbQK4V/photo-1581403341630-a6e0b9d2d257.jpg" />
@@ -38,6 +41,7 @@ const OurTeam = () => {
                     <p className="dark:text-gray-600">Manege </p>
                 </div>
             </div>
+            </Slide>
         </div>
     </section>
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,9 @@ import { AuthContext } from "../provider/AuthProvider";
 import { useLoaderData } from "react-router-dom";
 import LatestWorks from "../components/LatestWorks";
 import OurTeam from "../components/OurTeam";
+import { Typewriter } from 'react-simple-typewriter'
+
+
 
 const Home = () => {
 
@@ -19,26 +22,61 @@ const Home = () => {
     return (
         <div>
             <Benner></Benner>
-            <div className="lg:mx-24 mt-20  mb-8 lg:flex lg:gap-24 justify-stretch">
+            <div className="lg:mx-24 mt-20  mb-8 ">
             <div className="border-l-4 items-center content-center p-10 border-[#f314fa] ">
                 <p className="uppercase font-bold text-lg  text-[#8f33ff]">Art <span className="text-[#f314fa]">Lab</span></p>
                 {/* <p className="uppercase font-bold text-base text-[#f314fa]" >Provide</p> */}
             </div>
-            <div className="p-10 border-b-4 border-[#f314fa]  ">
-                <p className="uppercase font-bold text-base text-[#8f33ff]">Deliver</p>
-                <p className="uppercase font-bold text-base text-[#f314fa]" > Best Paintings</p>
+            <div className="py-5 px-10  w-56 border-b-4 border-[#f314fa]">
+                <p className="uppercase w-4/12 font-bold text-base text-[#8f33ff]">Deliver</p>
+                <p className="uppercase font-bold text-base text-[rgb(243,20,250)]" >
+                <span className="uppercase font-bold text-base text-[rgb(243,20,250)]" style={{  fontWeight: 'bold' }}>
+          {/* Style will be inherited from the parent element */}
+          <Typewriter 
+            words={[ 'Best Paintings']}
+            loop={100}
+            cursor
+            cursorStyle='_'
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={2000}
+          />
+        </span></p>
             </div>
-            <div className=" p-10 border-b-4 border-[#f314fa]  ">
+            <div className=" py-5 px-10  w-56  border-b-4 border-[#f314fa]  ">
                 <p className="uppercase font-bold text-base text-[#8f33ff]">Our Product</p>
-                <p className="uppercase  font-bold text-base text-[#f314fa]" >Latest & Original</p>
+                <p className="uppercase font-bold text-base text-[rgb(243,20,250)]" >
+                <span className="uppercase font-bold text-base text-[rgb(243,20,250)]" style={{  fontWeight: 'bold' }}>
+          {/* Style will be inherited from the parent element */}
+          <Typewriter 
+            words={[ 'Latest ']}
+            loop={100}
+            cursor
+            cursorStyle='_'
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={2000}
+          />
+        </span></p>
             </div>
-            <div className="border-b-4  p-10 border-[#f314fa]  ">
+            <div className="border-b-4 py-5 px-10 w-56 border-[#f314fa]  ">
                 <p className="uppercase font-bold text-base text-[#8f33ff]">We Provide</p>
-                <p className="uppercase font-bold text-base text-[#f314fa]" >Chip Cost</p>
+                <p className="uppercase font-bold text-base text-[rgb(243,20,250)]" >
+                <span className="uppercase font-bold text-base text-[rgb(243,20,250)]" style={{  fontWeight: 'bold' }}>
+          {/* Style will be inherited from the parent element */}
+          <Typewriter 
+            words={[ 'Chip Cost']}
+            loop={100}
+            cursor
+            cursorStyle='_'
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={2000}
+          />
+        </span></p>
             </div>
 
             </div>
-            
             <div className="gap-10 lg:mx-24 grid grid-cols-4 my-5">
                 {
                     items.slice(0, 8).map(item => <ItemsCart 
